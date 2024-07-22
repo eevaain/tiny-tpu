d5a37eba1c3862a7fe91cc181dc076138d53565c
Evan Lin <62809897+eevaain@users.noreply.github.com>
2024-07-22 16:03:47 -0700

    dub

--- src/tt_um_tpu.sv
+++ src/tt_um_tpu.sv
@@ -35,90 +35,90 @@ typedef enum reg [2:0] {
 
   state_t state = IDLE;
 
-    always @(posedge clk or posedge reset) begin
+    always @(*) begin
         if (reset) begin
-            fetch_w <= 0; 
-            fetch_inp <= 0;
-            fetch_ins <= 0;
-            start <= 0;
-            uio_oe_reg <= 8'b0000_0000; // All pins as inputs initially
+            fetch_w = 0; 
+            fetch_inp = 0;
+            fetch_ins = 0;
+            start = 0;
+            uio_oe_reg = 8'b0000_0000; // All pins as inputs initially
         end 
 
-        case (state)
-            IDLE: begin 
-                if (uio_in[7:5] == 3'b001) begin
-                    state <= FETCH_W; 
-                end else if (uio_in[7:5] == 3'b010) begin
-                    state <= FETCH_INP; 
-                end else if (uio_in[7:5] == 3'b011) begin
-                    state <= FETCH_INS; 
-                end else if (uio_in[7:5] == 3'b100) begin
-                    state <= START; 
-                end else begin
-                    state <= IDLE; 
-                end
-            end
-            FETCH_W: begin
-                fetch_inp <= 1; 
-                fetch_inp <= 0; 
-                fetch_ins <= 0;
-                start <= 0; 
-                state <= IDLE; 
-            end
-            FETCH_INP: begin
-                fetch_inp <= 0; 
-                fetch_inp <= 1; 
-                fetch_ins <= 0;
-                start <= 0; 
-                state <= IDLE; 
-            end
-            FETCH_INS: begin
-                fetch_inp <= 0; 
-                fetch_inp <= 0; 
-                fetch_ins <= 1;
-                start <= 0; 
-                state <= IDLE; 
-            end   
-            START: begin
-                fetch_inp <= 0; 
-                fetch_inp <= 0; 
-                fetch_ins <= 0;
-                start <= 1; 
-                state <= IDLE; 
-            end 
-        endcase
+        // case (state)
+        //     IDLE: begin 
+        //         if (uio_in[7:5] == 3'b001) begin
+        //             state = FETCH_W; 
+        //         end else if (uio_in[7:5] == 3'b010) begin
+        //             state = FETCH_INP; 
+        //         end else if (uio_in[7:5] == 3'b011) begin
+        //             state = FETCH_INS; 
+        //         end else if (uio_in[7:5] == 3'b100) begin
+        //             state = START; 
+        //         end else begin
+        //             state = IDLE; 
+        //         end
+        //     end
+        //     FETCH_W: begin
+        //         fetch_inp = 1; 
+        //         fetch_inp = 0; 
+        //         fetch_ins = 0;
+        //         start = 0; 
+        //         state = IDLE; 
+        //     end
+        //     FETCH_INP: begin
+        //         fetch_inp = 0; 
+        //         fetch_inp = 1; 
+        //         fetch_ins = 0;
+        //         start = 0; 
+        //         state = IDLE; 
+        //     end
+        //     FETCH_INS: begin
+        //         fetch_inp = 0; 
+        //         fetch_inp = 0; 
+        //         fetch_ins = 1;
+        //         start = 0; 
+        //         state = IDLE; 
+        //     end   
+        //     START: begin
+        //         fetch_inp = 0; 
+        //         fetch_inp = 0; 
+        //         fetch_ins = 0;
+        //         start = 1; 
+        //         state = IDLE; 
+        //     end 
+        // endcase
 
        
 
 
         
         
-        // else begin
-        //     fetch_w <= 0; 
-        //     fetch_inp <= 0;
-        //     fetch_ins <= 0;
-        //     start <= 0;
+        else begin
+            fetch_w = 0; 
+            fetch_inp = 0;
+            fetch_ins = 0;
+            start = 0;
 
-        //     case (uio_in[7:5]) 
-        //         3'b000: begin 
-        //             fetch_w <= 0; 
-        //             fetch_inp <= 0;
-        //             fetch_ins <= 0;
-        //             start <= 0;
-        //         end
-        //         3'b001: fetch_w <= 1; 
-        //         3'b010: fetch_inp <= 1;
-        //         3'b011: fetch_ins <= 1; 
-        //         3'b101: start <= 1; 
-        //         default: begin 
-        //             fetch_w <= 0; 
-        //             fetch_inp <= 0;
-        //             fetch_ins <= 0;
-        //             start <= 0;
-        //         end
-        //     endcase
+            case (uio_in[7:5]) 
+                3'b000: begin 
+                    fetch_w = 0; 
+                    fetch_inp = 0;
+                    fetch_ins = 0;
+                    start = 0;
+                end
+                3'b001: fetch_w = 1; 
+                3'b010: fetch_inp = 1;
+                3'b011: fetch_ins = 1; 
+                3'b101: start = 1; 
+                default: begin 
+                    fetch_w = 0; 
+                    fetch_inp = 0;
+                    fetch_ins = 0;
+                    start = 0;
+                end
+            endcase
 
-        // end
+        end
     end
 
 endmodule
